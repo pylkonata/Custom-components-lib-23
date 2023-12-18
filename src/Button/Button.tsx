39ea99711d131dd663bb16309b7467c43d6ba2e4
pylkonata <pylko.nata@gmail.com>
2023-12-19 00:36:04 +0200

    refactor: format props

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import s from './Button.module.scss';
 
-type variantBtn = 'text' | 'contained' | 'outlined';
-type sizeBtn = 'small' | 'medium' | 'large';
+type VariantBtn = 'text' | 'contained' | 'outlined';
+type SizeBtn = 'small' | 'medium' | 'large';
 
 interface ButtonProps {
-  variant: variantBtn,
-  size: sizeBtn,
-  onClick?: () => void,
-  disabled?: boolean,
-  children: React.ReactNode,
+	variant: VariantBtn;
+	size: SizeBtn;
+	onClick?: () => void;
+	disabled?: boolean;
+	children: React.ReactNode;
 }
-const Button = ({ variant = 'contained', size, onClick, disabled = false, children, ...props }: ButtonProps) => {
-  const classStyles = [s.btn, s[variant], s[size]];
-  return (
-    <button className={classStyles.join(' ')} disabled onClick={onClick} {...props} >
-      {children}
-    </button>
-  )
-}
+const Button = ({
+	variant = 'contained',
+	size,
+	onClick,
+	disabled = false,
+	children,
+	...props
+}: ButtonProps) => {
+	const classStyles = [s.btn, s[variant], s[size]];
+	return (
+		<button className={classStyles.join(' ')} disabled={disabled} onClick={onClick} {...props}>
+			{children}
+		</button>
+	);
+};
 
 export default Button;
