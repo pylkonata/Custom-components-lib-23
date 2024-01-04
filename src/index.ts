8ac20587def6eda7f8cf9525b6a9953ed75b8dc4
pylkonata <pylko.nata@gmail.com>
2024-01-04 02:21:35 +0200

    feat: add checkbox stories

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import Button from './Button/Button';
 import TextField from './TextField/TextField';
+import Checkbox from './Checkbox/Checkbox';
 
-export { Button, TextField };
+export { Button, TextField, Checkbox };
