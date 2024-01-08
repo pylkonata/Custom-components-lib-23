f86eddbe39315b260c2042d2eb09089c7ec8731c
pylkonata <pylko.nata@gmail.com>
2024-01-08 22:44:06 +0200

    feat: add switch component

---
+++ src/Switch/Switch.tsx
@@ -0,0 +1,42 @@
+import { ChangeEvent, useId, useState, MouseEvent } from 'react';
+import s from './Switch.module.scss';
+interface SwitchProps {
+	checked: boolean;
+	onChange: () => void;
+	label?: string;
+	disabled?: boolean;
+}
+
+const Switch = ({ disabled, checked, onChange, label }: SwitchProps) => {
+	const [isChecked, setIsChecked] = useState(checked);
+	const id = useId();
+	const onChangeSwitch = (event: ChangeEvent<HTMLInputElement> | MouseEvent<HTMLDivElement>) => {
+		if (event.currentTarget !== event.target) {
+			return;
+		}
+		setIsChecked((prev) => !prev);
+		onChange();
+	};
+	const wrapClasses = [s['wrapper'], disabled ? s['disabled'] : ''].join(' ');
+
+	return (
+		<div className={wrapClasses}>
+			<div className={s['container']} onClick={onChangeSwitch} tabIndex={-1}>
+				<input
+					type='checkbox'
+					id={`switch-${id}`}
+					className={s['switch']}
+					onChange={onChangeSwitch}
+					checked={isChecked}
+					disabled={disabled}
+				/>
+				<div className={s['circle']} />
+			</div>
+			<label htmlFor={`switch-${id}`} className={s['label']}>
+				{label}
+			</label>
+		</div>
+	);
+};
+
+export default Switch;
