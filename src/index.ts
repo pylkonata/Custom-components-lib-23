83c5e0d810bcef66625e4334d4b956e690ae6e13
pylkonata <pylko.nata@gmail.com>
2024-01-09 23:35:18 +0200

    feat: add modal stories, update exports

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import TextField from './TextField/TextField';
 import Select, { OptionType } from './Select/Select';
 import Checkbox from './Checkbox/Checkbox';
 import Switch from './Switch/Switch';
+import Modal from './Modal/Modal';
 
-export { Button, TextField, Select, OptionType, Checkbox, Switch };
+export { Button, TextField, Select, OptionType, Checkbox, Switch, Modal };
