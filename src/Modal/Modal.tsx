c9cfbcf4a0550571779893ad613a094013dde823
pylkonata <pylko.nata@gmail.com>
2024-01-10 00:03:01 +0200

    test: add unit tests and data attributes

--- src/Modal/Modal.tsx
+++ src/Modal/Modal.tsx
@@ -3,41 +3,41 @@ import { createPortal } from 'react-dom';
 import s from './Modal.module.scss';
 
 interface ModalProps {
-	open: boolean;
-	onClose: () => void;
-	children: React.ReactNode;
+  open: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
 }
 const Modal = ({ open, onClose, children }: ModalProps) => {
-	useEffect(() => {
-		if (open) {
-			document.body.style.overflow = 'hidden';
-			return () => {
-				document.body.style.overflow = 'visible';
-			};
-		}
-	}, [open]);
+  useEffect(() => {
+    if (open) {
+      document.body.style.overflow = 'hidden';
+      return () => {
+        document.body.style.overflow = 'visible';
+      };
+    }
+  }, [open]);
 
-	useEffect(() => {
-		const handleEsc = (e: KeyboardEvent) => {
-			if (e.key === 'Escape') {
-				onClose();
-			}
-		};
-		document.addEventListener('keyup', handleEsc);
+  useEffect(() => {
+    const handleEsc = (e: KeyboardEvent) => {
+      if (e.key === 'Escape') {
+        onClose();
+      }
+    };
+    document.addEventListener('keyup', handleEsc);
 
-		return () => {
-			document.removeEventListener('keyup', handleEsc);
-		};
-	}, []);
+    return () => {
+      document.removeEventListener('keyup', handleEsc);
+    };
+  }, []);
 
-	if (!open) return null;
-	return createPortal(
-		<div className={`${s['modalWrapper']} ${s['active']}`}>
-			<div className={s['backdrop']} onClick={onClose}></div>
-			<div className={s['modal']}>{children}</div>
-		</div>,
-		document.body
-	);
+  if (!open) return null;
+  return createPortal(
+    <div className={`${s['modalWrapper']} ${s['active']}`} data-testid='wrapper'>
+      <div className={s['backdrop']} onClick={onClose} data-testid='backdrop' />
+      <div className={s['modal']}>{children}</div>
+    </div>,
+    document.body
+  );
 };
 
 export default Modal;
